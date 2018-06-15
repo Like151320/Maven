0f15273494242a3cd599232857dc164b63b027ff
(o_o) <2523331265@qq.com>
2018-06-15 13:43:11 +0800

    0

--- app/src/main/java/like/maven/MainActivity.kt
+++ app/src/main/java/like/maven/MainActivity.kt
@@ -2,14 +2,11 @@ package like.maven
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import maven.like.testlibrary.TestPrint
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        TestPrint.printA()
     }
 }
---
+++ testlibrary/src/main/java/maven/like/testlibrary/TestPrint.java
@@ -0,0 +1,12 @@
+package maven.like.testlibrary;
+
+/**
+ * 作者: Li_ke
+ * 日期: 2018/6/15 13:38
+ * 作用:
+ */
+public class TestPrint {
+    public static void printA() {
+        System.out.print('A');
+    }
+}
