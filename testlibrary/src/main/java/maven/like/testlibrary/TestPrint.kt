58540c74e8929e6065d06e7757dc49eba08764bf
(o_o) <2523331265@qq.com>
2018-06-15 11:15:26 +0800

    Initial commit

---
+++ testlibrary/src/main/java/maven/like/testlibrary/TestPrint.kt
@@ -0,0 +1,14 @@
+package maven.like.testlibrary
+
+import android.util.Log
+
+/**
+ * 作者: Li_ke
+ * 日期: 2018/6/15 10:47
+ * 作用:
+ */
+object TestPrint {
+    fun printA() {
+        Log.i("Test", "A")
+    }
+}
